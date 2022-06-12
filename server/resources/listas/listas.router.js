8d56df794ea8e523eed30cfcc12b9de532ce51f9
Zhomas06 <tomasnavasgutierrez@gmail.com>
2022-06-12 13:35:35 +0200

    minor changes

--- server/resources/listas/listas.router.js
+++ server/resources/listas/listas.router.js
@@ -4,7 +4,6 @@ const controlator = require("./listas.controller");
 
 router.route("/listas").get(controlator.findAll);
 router.route("/listas/:id").get(controlator.findOne).delete(controlator.deleteOne);
-
 router.route("/listas/:id/items").delete(controlator.deleteElementList).post(controlator.addingElementList);
 
 module.exports = router;
